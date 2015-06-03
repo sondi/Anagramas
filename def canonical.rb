722c06106effde10020f999e10f26a085c3b45ee
codeamac02 <codeamac02@gmail.com>
2015-06-03 10:55:01 -0500

    Implementamos método anagrams_for

--- def canonical.rb
+++ def canonical.rb
@@ -7,4 +7,37 @@ def are_anagrams?(word1, word2)
   canonical(word1) == canonical(word2)
 end
 
-puts are_anagrams?("AMOR", "roma")
+def anagrams_for(string,array)
+	array.select{|word| are_anagrams?(string ,word)}
+end
+
+diccionario = ["AMOR", "ROMA", "OMAR", "MORA", "RAMO", "ARMO", "MARO","casa", "Dia", "AZUL"]
+
+p anagrams_for("Mora", diccionario)
+
+
+# Explicación de Métodos de Clase
+
+class Dog
+
+	def bark
+		"wow"
+	end
+
+
+	def bark_loud
+		shout = bark()
+		shout.upcase()
+	end
+
+end
+
+Fido = Dog.new()
+
+puts Fido.bark
+puts Fido.bark_loud
+
+
+
+
+
