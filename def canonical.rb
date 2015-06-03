17c7a5c0bf8a145c52ff3189ad563af07ce7ec3a
codeamac02 <codeamac02@gmail.com>
2015-06-03 10:48:18 -0500

    Implementamos método canonical

--- def canonical.rb
+++ def canonical.rb
@@ -1,7 +1,10 @@
-# def canonical(word)
-#   # implementa el formato canonical
-# end
+def canonical(word)
+  puts raise "Erorr: I do not recive numebrs I recive strings" if word.class == Fixnum
+  word.downcase.chars.sort.join
+end
 
 def are_anagrams?(word1, word2)
   canonical(word1) == canonical(word2)
 end
+
+puts are_anagrams?("AMOR", "roma")
